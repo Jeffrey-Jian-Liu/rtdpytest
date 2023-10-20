3f0545b2d4cf4ea2cbf810a0b1e3f9dd8a88bc8e
Jeffrey Liu <33611314+Jeffrey-Jian-Liu@users.noreply.github.com>
2023-10-21 10:27:18 +1100

    Create fxn.py

---
+++ src/lang/fxn.py
@@ -0,0 +1,53 @@
+"""Fxn Module"""
+
+"""
+Description: Python Function Test Module
+Author     : Jeffrey Liu
+Version    : 2023.08.11.01
+"""
+
+
+class Fxn:
+    """Fxn Class"""
+
+    def __init__(self):
+        """Init Fxn"""
+        self.name = None
+
+    def print(self):
+        """Print Fxn"""
+        self.name = 'Fxn Class'
+        print('This is ' + self.name)
+
+    @staticmethod
+    def first(**kwargs):
+        def outer(fl2):
+            def inner():
+                if kwargs['ist']:
+                    print('Test!')
+                if kwargs['isp']:
+                    print('Print!')
+                fl2()
+
+            return inner
+
+        return outer
+
+    @staticmethod
+    def second(fl1):
+        def inner():
+            print('second')
+            fl1()
+
+        return inner
+
+    @staticmethod
+    @first(ist=False, isp=True)
+    @second
+    def third():
+        print('hello')
+
+
+if __name__ == '__main__':
+    fxn = Fxn()
+    fxn.third()
