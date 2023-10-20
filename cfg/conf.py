4be3dd4b0e0a1e2b98c7c2606c2968228508d49a
Jeffrey Liu <33611314+Jeffrey-Jian-Liu@users.noreply.github.com>
2023-10-20 19:11:13 +1100

    Update conf.py

--- cfg/conf.py
+++ cfg/conf.py
@@ -8,6 +8,7 @@ release = '2023.10.20'
 version = '2023.10.20.1'
 
 # -- General configuration ---------------------------------------------------
+root_doc = '../src'
 extensions = []
 
 templates_path = ['_templates']
