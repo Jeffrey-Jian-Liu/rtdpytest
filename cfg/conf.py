0f92e23c912f8600322cc8e82e0e9167ce73deef
Jeffrey Liu <33611314+Jeffrey-Jian-Liu@users.noreply.github.com>
2023-10-20 18:06:18 +1100

    Create conf.py

---
+++ cfg/conf.py
@@ -0,0 +1,17 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+project = 'pytest'
+copyright = '2023, lzxn'
+author = 'lzxn'
+release = '2023.10.20.1'
+
+# -- General configuration ---------------------------------------------------
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
