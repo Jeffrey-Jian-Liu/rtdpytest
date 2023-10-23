fa18e7a51576fd78ddfb5c94835ca3377c35ad5d
Jeffrey Liu <jeffrey.jian.liu.au@gmail.com>
2023-10-23 17:01:12 +1100

    update

--- cfg/conf.py
+++ cfg/conf.py
@@ -1,6 +1,7 @@
 # -- Source path setup -------------------------------------------------------
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
